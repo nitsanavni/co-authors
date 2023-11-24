86450c276a9648f8947147ba081bfe8d8686b85a
Nitsan Avni <nitsanav@gmail.com>
2023-11-24 17:41:27 +0100

    refine prompt, support ~/.co-authors

--- co-authors.py
+++ co-authors.py
@@ -40,19 +40,27 @@ class CoAuthor(BaseModel):
     name: str
     email: str
 
+    @staticmethod
+    def from_string(s: str):
+        return CoAuthor(
+            name=s.split("<")[0].strip().split(":")[1].strip(),
+            email=s.split("<")[1].split(">")[0].strip(),
+        )
+
     def __str__(self):
         return f"Co-authored-by: {self.name} <{self.email}>"
 
 
 @marvin.ai_fn
-def determine_co_authors(
+def determine_new_co_authors(
     user_input: str, current_co_authors: str, known_co_authors: str
 ) -> list[CoAuthor]:
     """
-    Determines the co-authors based on
+    Determines the new set of co-authors based on
     the user input and
     the current co-authors and
     known co-authors.
+    note: when the user says "add" new person, return current + new
     """
 
 
@@ -70,17 +78,17 @@ def get_current_co_authors():
 def read_co_authors_from_git_history():
     try:
         authors = subprocess.check_output(
-            "git log --pretty=format:'%an <%ae>' | sort | uniq",
+            "git log --pretty=format:'%an <%ae>' | uniq",
             shell=True,
             text=True,
-        )
-        print("Authors from git history:", authors, file=sys.stderr)
+        ).split("\n")
+        authors = "\n".join(dict.fromkeys(authors))
         co_authors_output = subprocess.check_output(
-            "git log --pretty=format:'%B' | grep -i 'Co-authored-by' | sort | uniq || true",
+            "git log --pretty=format:'%B' | grep -i 'Co-authored-by' | uniq || true",
             shell=True,
             text=True,
-        )
-        print("Co-authors from git history:", co_authors_output, file=sys.stderr)
+        ).split("\n")
+        co_authors_output = "\n".join(dict.fromkeys(co_authors_output))
 
         return "\n".join([authors, co_authors_output])
     except subprocess.CalledProcessError as e:
@@ -107,7 +115,6 @@ def format_co_authors(co_authors: list[CoAuthor]) -> str:
 
 def update_co_authors(co_authors: list[CoAuthor]):
     new_co_authors = format_co_authors(co_authors)
-    print("New co-authors:", new_co_authors, file=sys.stderr)
     with open(local_co_authors_file, "w") as f:
         f.write(new_co_authors)
 
@@ -117,7 +124,11 @@ def config_git_to_use_commit_template():
 
 
 def git_ignore_co_authors_file():
-    os.system(f"echo {local_co_authors_file} >> .gitignore")
+    if not exists(file=".gitignore"):
+        os.system("touch .gitignore")
+    if local_co_authors_file in open(".gitignore").read():
+        return
+    os.system(f'echo "\n{local_co_authors_file}" >> .gitignore')
 
 
 def set_up():
@@ -125,15 +136,30 @@ def set_up():
     git_ignore_co_authors_file()
 
 
+def parse(co_authors: str = "") -> list[CoAuthor]:
+    if not co_authors:
+        return []
+    return [CoAuthor.from_string(s) for s in co_authors.split("\n")]
+
+
+def update_global_co_authors(co_authors: list[CoAuthor]):
+    global_co_authors = parse(read_co_authors_file(file=global_co_authors_file))
+    new_entries = {
+        str(co_author): co_author for co_author in global_co_authors + co_authors
+    }.values()
+    with open(global_co_authors_file, "w") as f:
+        f.write(format_co_authors(new_entries))
+
+
 def main():
     set_up()
-    update_co_authors(
-        co_authors=determine_co_authors(
-            user_input=get_user_input(),
-            current_co_authors=get_current_co_authors(),
-            known_co_authors=get_known_co_authors(),
-        )
+    co_authors = determine_new_co_authors(
+        user_input=get_user_input(),
+        current_co_authors=get_current_co_authors(),
+        known_co_authors=get_known_co_authors(),
     )
+    update_co_authors(co_authors=co_authors)
+    update_global_co_authors(co_authors=co_authors)
 
 
 if __name__ == "__main__":
